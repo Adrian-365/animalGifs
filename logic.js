94ec0ac4d1ee318dbc05acd9a469b60db80d6588
Adrian Nalwalker <analwalker@planetfg.local>
2017-11-19 11:26:27 -0800

    added ajax req and url and api key

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-var animals = ['lion', 'tiger', 'bear', 'wolf', 'crockodile', 'hippo', 'giraffe', 'bison', 'dog', 'cat', 'elk', 'quail', 'deer', 'panther', 'mountain lion' ];
+var animals = ['lion', 'tiger', 'bear', 'wolf', 'crockodile', 'hippo', 'giraffe', 'bison', 'elk', 'quail', 'deer', 'panther', 'mountain lion' ];
 
 $(document).ready(function () {
   //your code here
@@ -15,11 +15,12 @@ animals.forEach(function(each) {
 });
 
 //event listener for "submit" button
-$('#submit').on('click', function() {
+$('#submit').on('click', function(event) {
+	event.preventDefault();
 	// console.log('clicked!!');
 	//grab text from the input field 
 	var newAnimal = $('#input').val();
-	// console.log(newAnimal);
+	console.log(newAnimal);
 	//add text from the input field to the array
 	animals.push(newAnimal);
 	// console.log(animals);
@@ -38,17 +39,32 @@ $('#submit').on('click', function() {
 $('.animal-button').on('click', function() {
 	//grab the data value from the button and put it in a variable
 	var thisAnimal = $(this).data('animal');
+	//console.log only works for original buttons not for new.??
 	console.log(thisAnimal);
 	//create url variable
+	var queryUrl = 'http://api.giphy.com/v1/gifs/search?q=' + thisAnimal + '&api_key=GVCTd0mGVPvvKq31RpOcdnKSIexmVIDU&limit=1';
 
-	//ajax call get
+	//ajax call get/then
+	$.ajax({
+		url: queryUrl,
+		method: "GET"
+	}).then(function(response) {
+		console.log(response);
+	// clear existing gifs, 
+	$('#gifs').empty();
 
-	//ajax .then to clear existing gifs, 
+	
 
-	//then populate a 10 gifs with their g/pg rating
+	// //then populate a 10 gifs with their g/pg rating
+	// var picDiv = $('<img>');
+	// picDiv.addClass('col-xs-6 col-md-3 thumbnail');
+	// picDiv.addSrc('');
+
+
+		
 	//include click start and stop using data-
 
-
+	});
 
 });
 

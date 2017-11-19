81ee1b2ab5f22773b217d9323349c04fcf63633f
Adrian Nalwalker <analwalker@planetfg.local>
2017-11-18 18:19:10 -0800

    added listerner to the submit button and it works

--- logic.js
+++ logic.js
@@ -1,10 +1,28 @@
-var animals = ['lion', 'tiger', 'bear', 'wolf', 'crockodile', 'hippo', 'giraffe', 'bison', 'dog', 'cat', 'elk', 'quail', 'deer', 'panther', 'mouontain lion' ];
+var animals = ['lion', 'tiger', 'bear', 'wolf', 'crockodile', 'hippo', 'giraffe', 'bison', 'dog', 'cat', 'elk', 'quail', 'deer', 'panther', 'mountain lion' ];
 
+$(document).ready(function () {
+  //your code here
+
+//build buttons from the array:
 animals.forEach(function(each) {
-	console.log(each);
+	// console.log(each);
 	var b = $('<button>');
 	b.addClass('animal-button btn btn-default btn-lg');
-	b.attribute('data-animal', each);
+	b.attr('data-animal', each);
 	b.text(each);
 	$('#buttons').append(b);
+});
+
+//event listener for "submit" button
+$('#submit').on('click', function() {
+	// console.log('clicked!!');
+	//grab text from the input field and add it to the array
+	var newAnimal = $('#input').value;
+	console.log(newAnimal);
+
+});
+
+
+
+//end dcoument.ready function
 });
